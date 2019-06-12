54af3854010a349ab15aa742ed7e208bdf9b41bf
Color_yr <402067010@qq.com>
2019-06-12 20:11:07 +0800

    up

---
+++ Native.Csharp/Minecraft_QQ/messagelist.cs
@@ -0,0 +1,10 @@
+﻿namespace Color_yr.Minecraft_QQ
+{
+    public class messagelist
+    {
+        public string group = null;
+        public string player = null;
+        public string message = null;
+        public bool is_commder = false; 
+    }
+}
