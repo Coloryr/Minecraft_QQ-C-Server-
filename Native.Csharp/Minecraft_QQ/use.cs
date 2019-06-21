aa9a6b455f4e7f4e4c4b7ecee75c76adacb1c9c4
Color_yr <402067010@qq.com>
2019-06-21 16:37:25 +0800

    beta8

--- Native.Csharp/Minecraft_QQ/use.cs
+++ Native.Csharp/Minecraft_QQ/use.cs
@@ -466,7 +466,13 @@ namespace Color_yr.Minecraft_QQ
                         }
                         messagelist.is_commder = true;
                         if (XML.read_memory(config_read.commder_m, "指令" + i.ToString(), "玩家发送") == "是")
-                            messagelist.player = fromQQ.ToString();
+                        {
+                            messagelist.player = check_player_name(fromQQ.ToString());
+                            if(messagelist.player == null)
+                                Common.CqApi.SendGroupMessage(fromGroup, Common.CqApi.CqCode_At(fromQQ) + "你未绑定ID");
+                            return true;
+                        }
+                            
                         else
                             messagelist.player = "后台";
                         socket.Send(messagelist);
