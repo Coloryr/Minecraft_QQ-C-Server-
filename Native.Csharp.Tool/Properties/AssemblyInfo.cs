2f246faef24326dd10876470fe4af9ddf33fa3ab
Color_yr <402067010@qq.com>
2019-07-17 22:18:16 +0800

    up

--- Native.Csharp.Tool/Properties/AssemblyInfo.cs
+++ Native.Csharp.Tool/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Navite.Csharp.Tool")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("3.0.2.0705")]
-[assembly: AssemblyFileVersion ("3.0.2.0705")]
+[assembly: AssemblyVersion("3.0.4.0716")]
+[assembly: AssemblyFileVersion("3.0.4.0716")]
