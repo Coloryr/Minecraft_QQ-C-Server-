1d38c81869207dd58773eeb4990d14ddb9d3a9ae
Coloryr <402067010@qq.com>
2020-01-15 18:32:54 +0800

    同步框架

--- Minecraft_QQ/logs.cs
+++ Minecraft_QQ/logs.cs
@@ -6,16 +6,20 @@ namespace Color_yr.Minecraft_QQ
     public class logs
     {
         public static string log = "logs.log";
+        private static object obj = new object();
 
         public static void Log_write(string a)
         {
             try
             {
-                DateTime date = DateTime.Now;
-                string year = date.ToShortDateString().ToString();
-                string time = date.ToLongTimeString().ToString();
-                string write = "[" + year + "]" + "[" + time + "]" + a;
-                File.AppendAllText(Minecraft_QQ.path + log, write + Environment.NewLine);
+                lock(obj)
+                {
+                    DateTime date = DateTime.Now;
+                    string year = date.ToShortDateString().ToString();
+                    string time = date.ToLongTimeString().ToString();
+                    string write = "[" + year + "]" + "[" + time + "]" + a;
+                    File.AppendAllText(Minecraft_QQ.path + log, write + Environment.NewLine);
+                }
             }
             catch
             {
