42a11ab539678357ffb6fff6fa92579539db5875
Coloryr <402067010@qq.com>
2020-03-29 11:47:53 +0800

    up

---
+++ Minecraft_QQ/Form/SetWPF/QQSet.xaml.cs
@@ -0,0 +1,28 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Minecraft_QQ.Form.SetWPF
+{
+    /// <summary>
+    /// QQSet.xaml 的交互逻辑
+    /// </summary>
+    public partial class QQSet : MetroWindow
+    {
+        public QQSet()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Minecraft_QQ/Form/Window1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Minecraft_QQ.Form
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1 : MetroWindow
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeQQ(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteQQ(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
