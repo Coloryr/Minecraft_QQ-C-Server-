8ffbbe835a3ef12503ace0dff764705e6311be18
Color_yr <402067010@qq.com>
2020-07-07 16:50:29 +0800

    mysql fix

--- Native.Core/Export/CQEventExport.cs
+++ Native.Core/Export/CQEventExport.cs
@@ -64,7 +64,7 @@ namespace Native.App.Export
 			// 反射获取 AppData 实例	
 			Type appDataType = typeof (AppData);	
 			// 注册一个 CQApi 实例	
-			AppInfo appInfo = new AppInfo ("com.color_yr.minecraft_qq", 1, 9, "Minecraft_QQ", "2.7.5.0", 1, "Color_yr", "MC服务器与QQ群互通", authCode);	
+			AppInfo appInfo = new AppInfo ("com.color_yr.minecraft_qq", 1, 9, "Minecraft_QQ", "2.9.0.2", 1, "Color_yr", "MC服务器与QQ群互通", authCode);	
 			appDataType.GetRuntimeProperty ("CQApi").GetSetMethod (true).Invoke (null, new object[] { new CQApi (appInfo) });	
 			AppData.UnityContainer.RegisterInstance<CQApi> ("com.color_yr.minecraft_qq", AppData.CQApi);	
 			// 向容器注册一个 CQLog 实例	
