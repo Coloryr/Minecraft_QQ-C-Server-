7468987869233f313e7d596018ae8460e26ab3ca
Color_yr <402067010@qq.com>
2020-08-11 23:16:14 +0800

    今天先这样

--- Minecraft_QQ_Gui/App.xaml.cs
+++ Minecraft_QQ_Gui/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Minecraft_QQ_Core;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -15,6 +16,17 @@ namespace Minecraft_QQ_Gui
     {
         public static System.Windows.Forms.NotifyIcon notifyIcon;
 
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            notifyIcon = new System.Windows.Forms.NotifyIcon();
+            notifyIcon.Visible = true;
+            notifyIcon.Click += NotifyIcon_Click;
+            IMinecraft_QQ.Start();
+        }
 
+        private void NotifyIcon_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
